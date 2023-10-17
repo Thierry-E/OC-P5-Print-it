cb42b08cd59d80e31ff872de12f5062fd609658b
Thierry-E <76159642+Thierry-E@users.noreply.github.com>
2023-10-17 17:46:09 +0200

    Mise en place des étapes N°2 et N°3

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,30 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+  {
+    image: 'slide1.jpg',
+    tagLine: 'Impressions tous formats <span>en boutique et en ligne</span>',
+  },
+  {
+    image: 'slide2.jpg',
+    tagLine:
+      'Tirages haute définition grand format <span>pour vos bureaux et events</span>',
+  },
+  {
+    image: 'slide3.jpg',
+    tagLine: 'Grand choix de couleurs <span>de CMJN aux pantones</span>',
+  },
+  {
+    image: 'slide4.png',
+    tagLine: 'Autocollants <span>avec découpe laser sur mesure</span>',
+  },
 ]
+
+/*** Étape 2 : Ajouts des Event Listeners sur les flèches ***/
+let arrow_left = document.querySelector('.arrow_left')
+arrow_left.addEventListener('click', () => {
+  console.log("J'ai cliqué sur la flêche de gauche !")
+})
+
+let arrow_right = document.querySelector('.arrow_right')
+arrow_right.addEventListener('click', () => {
+  console.log("J'ai cliqué sur la flêche de droite !")
+})
